ec35c2a46a7f2169ced6f3076f97c76321c4b6cc
vitu1415 <vitoralves0801@gmail.com>
2025-03-16 22:55:55 -0300

    vs6-atualizacao do CORS

--- src/main/java/com/ract/testreact/config/TestConfig.java
+++ src/main/java/com/ract/testreact/config/TestConfig.java
@@ -21,7 +21,8 @@ public class TestConfig implements CommandLineRunner, WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite CORS em todas as rotas
-                .allowedOrigins("http://localhost:3000") // Substitua pela sua origem
+                .allowedOrigins("http://localhost:3000",
+						"https://listsfuncionario.vercel.app") // Substitua pela sua origem
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*"); // Todos os cabeçalhos são permitidos
     }
